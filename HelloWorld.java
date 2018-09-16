93d90c21ccee0c83aa4dba4f3d97d62cdd64459a
kfang00 <34457735+kfang00@users.noreply.github.com>
2018-09-16 17:55:21 -0400

    Deleting more old files

--- HelloWorld.java
+++
@@ -1,5 +0,0 @@
-public class HelloWorld{
-    public static void main(String[] args){
-        System.out.println("Hello my name is Kayla Fang, I went on a cruise to Bermuda and overpaid for a popsicle.");
-    }
-}
