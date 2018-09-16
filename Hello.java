7713e207084373a301aa10e2fe71249dac87c7bc
Kayla Fang <kfang00@stuy.edu>
2018-09-16 17:52:17 -0400

    renamed my files

---
+++ Hello.java
@@ -0,0 +1,5 @@
+public class Hello{
+    public static void main(String[] args){
+        System.out.println("Hello my name is Kayla Fang, I went on a cruise to Bermuda and overpaid for a popsicle.");
+    }
+}
