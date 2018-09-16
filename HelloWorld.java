30f3cd8c5460f8b9dcf094ac0566e80e5e061a7d
Kayla Fang <kfang00@stuy.edu>
2018-09-16 17:42:52 -0400

    added code for HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello my name is Kayla Fang, I went on a cruise to Bermuda and overpaid for a popsicle.");
+    }
+}
